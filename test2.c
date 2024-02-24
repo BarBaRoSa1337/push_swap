7d9afaed10bed530f5053895c4b598327500b2bb
achakour1337 <you@example.com>
2024-02-24 19:34:30 +0100

    free palestine3032

--- test2.c
+++ test2.c
@@ -1,70 +1,73 @@
 #include "push_swap.h"
 int *modify_arr(int *arr, int n, int len);
-void    select_move2(push **stack_a, push **stack_b, int *pos)
-{
-    push    *last;
 
-        if (pos[1] == 1)
-            while (pos[0] > 0 && pos[0]--)
-                rra_rrb_rrr(stack_a, &last, "rrb");
-        else if (pos[1] == -1)
-            while (pos[0] > 0 && pos[0]--)
-                ra_rb_rr(stack_a, "rrb");
-        if (pos[3] == 1)
-            while (pos[2] > 0 && pos[2]--)
-                rra_rrb_rrr(stack_a, &last, "rra");
-        else if (pos[3] == -1)
-            while  (pos[2] > 0 && pos[2]--)
-                ra_rb_rr(stack_a, "ra");
-        pa_pb(stack_a, stack_b, "pb");
-}
+// void    select_move2(push **stack_a, push **stack_b, int *pos, int **arr)
+// {
+//     push    *last;
 
-void    select_move1(push **stack_a, push **stack_b, int *pos)
-{
-    push    *last;
-    int *arr = ft_fill_arr(*stack_a);
+//         if (pos[1] == 1)
+//             while (pos[0] > 0 && pos[0]--)
+//                 rra_rrb_rrr(stack_a, &last, "rrb");
+//         else if (pos[1] == -1)
+//             while (pos[0] > 0 && pos[0]--)
+//                 ra_rb_rr(stack_a, "rrb");
+//         if (pos[3] == 1)
+//             while (pos[2] > 0 && pos[2]--)
+//                 rra_rrb_rrr(stack_a, &last, "rra");
+//         else if (pos[3] == -1)
+//             while  (pos[2] > 0 && pos[2]--)
+//                 ra_rb_rr(stack_a, "ra");
+//         *arr =  modify_arr(*arr, last->data, ft_lstsize(*stack_a));
+//         pa_pb(stack_a, stack_b, "pb");
+// }
 
-    if (pos[1] == 1 && pos[3] == 1)
-    {
-        while (pos[0] > 0 && pos[2] > 0 && pos[0]-- && pos[2]--)
-            rr_rrr(stack_a, stack_b, "rrr");
-        while (pos[0] > 0 && pos[0]--)
-            rra_rrb_rrr(stack_b, &last, "rrb");
-        while (pos[2] > 0 && pos[2]--)
-            rra_rrb_rrr(stack_a , &last, "rra");
-        modify_arr(arr, last->data, ft_lstsize(*stack_a) + 1);
-        pa_pb(stack_a, stack_b, "pb");
-    }
-    else if (pos[1] == -1 && pos[3] == -1)
-    {
-        while (pos[0] > 0 && pos[2] > 0 && pos[0]-- && pos[2]--)
-            rr_rrr(stack_a, stack_b, "rrr");
-        while (pos[0] > 0 && pos[0]--)
-            ra_rb_rr(stack_b, "rb");
-        while (pos[2] > 0 && pos[2]--)
-            ra_rb_rr(stack_a, "ra");
-        pa_pb(stack_a, stack_b, "pb");
-    }
-    else
-        select_move2(stack_a, stack_b, pos);
-}
+// void    select_move1(push **stack_a, push **stack_b, int *pos)
+// {
+//     push    *last;
+//     int *arr = ft_fill_arr(*stack_a);
 
-void    selsect_moves(push **stack_a, push **stack_b, int *arr)
-{
-    push    *head;
-    int     *pos;
-    int b_len;
-    int index;
+//     if (pos[1] == 1 && pos[3] == 1)
+//     {
+//         while (pos[0] > 0 && pos[2] > 0 && pos[0]-- && pos[2]--)
+//             rr_rrr(stack_a, stack_b, "rrr");
+//         while (pos[0] > 0 && pos[0]--)
+//             rra_rrb_rrr(stack_b, &last, "rrb");
+//         while (pos[2] > 0 && pos[2]--)
+//             rra_rrb_rrr(stack_a , &last, "rra");
+//         *arr =  modify_arr(*arr, last->data, ft_lstsize(*stack_a));
+//         pa_pb(stack_a, stack_b, "pb");
+//     }
+//     else if (pos[1] == -1 && pos[3] == -1)
+//     {
+//         while (pos[0] > 0 && pos[2] > 0 && pos[0]-- && pos[2]--)
+//             rr_rrr(stack_a, stack_b, "rrr");
+//         while (pos[0] > 0 && pos[0]--)
+//             ra_rb_rr(stack_b, "rb");
+//         while (pos[2] > 0 && pos[2]--)
+//             ra_rb_rr(stack_a, "ra");
+//             *arr =  modify_arr(*arr, last->data, ft_lstsize(*stack_a));
+//         pa_pb(stack_a, stack_b, "pb");
+//     }
+//     else
+//         select_move2(stack_a, stack_b, pos, arr);
+// }
 
-    head = *stack_b;
-    b_len = ft_lstsize(*stack_b);
-    index = select_cheapest(*stack_a, *stack_b, arr ,b_len);
-    while (index--)
-        head = head->next;
-    pos = count_push_price(*stack_a, *stack_b, head->data, arr,b_len);
-    select_move1(stack_a, stack_b, pos);
-    free (pos);
-}
+// void    selsect_moves(push **stack_a, push **stack_b, int *arr)
+// {
+//     push    *head;
+//     int     *pos;
+//     int b_len;
+//     int index;
+
+//     head = *stack_b;
+//     b_len = ft_lstsize(*stack_b);
+//     index = select_cheapest(*stack_a, *stack_b, arr ,b_len);
+//     while (index--)
+//         head = head->next;
+//     pos = count_push_price(*stack_a, *stack_b, head->data, arr,b_len);
+//     select_move1(stack_a, stack_b, pos);
+//     free (pos);
+// }
 
 int put_numbr_inplace(int *arr, int n, int len)
 {
@@ -87,13 +90,13 @@ int *modify_arr(int *arr, int n, int len)
     new = malloc(sizeof(int) * len + 1);
     if (!new)
         return (0);
-    while (arr[i] && j < len  && arr[i] < n)
+    while (i < len && arr[i] < n)
         new[j++] = arr[i++];
-    if (arr[i] > n)
+    if (i < len && arr[i] > n)
         new[j++] = n;
-    while (arr[i])
+    while (i < len)
         new[j++] = arr[i++];
-    if (new[j - 1] < n)
+    if (j < len + 1 && new[j - 1] < n)
         new[j] = n;
     return (free (arr), new);
 }
@@ -116,10 +119,10 @@ int main(int ac, char **ar)
     b1->next = b2;
 
     int *arr = ft_fill_arr(n);
-    for (int i = 0; i < ft_lstsize(n); i++)
-    {
-        printf("1..%d\n", arr[i]);
-    }
+    // for (int i = 0; i < ft_lstsize(n); i++)
+    // {
+    //     printf("1..%d\n", arr[i]);
+    // }
     arr = modify_arr(arr, 4, ft_lstsize(n));
     for (int i = 0; i < ft_lstsize(n) + 1; i++)
     {
